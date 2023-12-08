2ca785423d689f3d10b3d1f2cf7d445098a6de91
Logan Lowder <loganlowder@gmail.com>
2023-12-07 20:53:34 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Logan's Personal Nix Config
+[WIP]
+
+I'm trying to learn nix by setting up a new MBP with nix, nix-darwin, and home-manager. 
+I'm very much still learning how all these tools work.
---
+++ flake.nix
@@ -0,0 +1,113 @@
+{
+  description = "llowder nix config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    darwin.url = "github:lnl7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs: {
+    darwinConfigurations.llowder =
+      inputs.darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        
+        pkgs = import inputs.nixpkgs { 
+          system = "aarch64-darwin";
+          config = { allowUnfree = true; };
+        };
+
+        modules = [
+          ({ pkgs, ... }: {
+            # backwards compatibility -- don't change
+            system.stateVersion = 4;
+            
+            environment.loginShell = pkgs.zsh;
+            environment.pathsToLink = [ "/Applications" ];
+            environment.systemPackages = [ 
+              pkgs.discord
+              pkgs.docker
+              pkgs.git
+              pkgs.htop
+              pkgs.jq
+              pkgs.lazygit
+              pkgs.nixpkgs-fmt
+              pkgs.tmux
+              pkgs.vscode
+            ];
+            environment.systemPath = [ "/opt/homebrew/bin" ];
+            
+            fonts.fonts = [ (pkgs.nerdfonts.override { fonts = [ "Hack" ]; }) ];
+            
+            programs.zsh.enable = true;
+            
+            services.nix-daemon.enable = true;
+            
+            system.defaults.dock.autohide = true;
+            system.defaults.finder.AppleShowAllExtensions = true;
+            system.defaults.NSGlobalDomain.AppleShowAllExtensions = true;
+            system.defaults.NSGlobalDomain.InitialKeyRepeat = 14;
+            system.defaults.NSGlobalDomain.KeyRepeat = 1;
+            system.keyboard.enableKeyMapping = true;
+            system.keyboard.remapCapsLockToEscape = true;
+            
+            users.users.llowder.home = "/Users/llowder";
+            
+            homebrew = {
+              enable = true;
+              casks = [ "signal" ];
+              #   brews = [ "trippy" ];
+              #   caskArgs.no_quarantine = true;
+              #   global.brewfile = true;
+              #   masApps = { };
+              #   taps = [ "fujiapple852/trippy" ];
+            };
+          })
+
+          inputs.home-manager.darwinModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.llowder.imports = [
+                ({ pkgs, ... }: {
+                  home.stateVersion = "22.11";
+                  home.sessionVariables = {
+                    PAGER = "less";
+                    CLICLOLOR = 1;
+                  };
+
+                  programs.starship.enable = true;
+                  programs.starship.enableZshIntegration = true;
+                  programs.zsh.enable = true;
+                  programs.zsh.enableAutosuggestions = true;
+                  programs.zsh.enableCompletion = true;
+                  programs.zsh.shellAliases = { ls = "ls --color=auto -F"; };
+                  programs.zsh.syntaxHighlighting.enable = true;
+                  programs.git = {
+                    enable = true;
+                    extraConfig = { init = { defaultBranch = "main"; }; };
+                    userName  = "Logan Lowder";
+                    userEmail = "loganlowder@gmail.com";
+                  };
+                  programs.ssh = {
+                    enable = true;
+                    extraConfig = ''
+                      Host github.com
+                        AddKeysToAgent yes
+                        UseKeychain yes
+                        IdentityFile ~/.ssh/id_ed25519
+                    '';
+                  };
+                })
+              ];
+            };
+          }
+        ];
+      };
+  };
+}
